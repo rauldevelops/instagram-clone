7e2d1de71b76c369be500ad75b358c7046c2ca3c
Raul Develops <124627746+rauldevelops@users.noreply.github.com>
2025-12-20 19:57:21 -0500

    fixed JSON data formatting, started building sendRes util function

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ import http from 'http'
 const PORT = 8000
 
 const server = http.createServer((req, res) => {
-    console.log('hello from the server')
+    
+    if (req.url === '/') {
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/html')
+    res.end('<h1>Hello, World!</h1>')
+    }
 })
 
 server.listen(PORT, () => {
-   
-    
+    console.log(`Server is listening on port ${PORT}`)
 })
---
+++ utils/sendResponse.js
@@ -0,0 +1,3 @@
+export async function sendResponse(res, statusCode, contentType, payload) {
+
+}
