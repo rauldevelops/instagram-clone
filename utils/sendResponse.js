8f7f46552c6d968f7af152a67449a1cf23e46952
Raul Develops <124627746+rauldevelops@users.noreply.github.com>
2025-12-20 21:42:59 -0500

    added a few more util functions

--- utils/sendResponse.js
+++ utils/sendResponse.js
@@ -1,3 +1,5 @@
 export async function sendResponse(res, statusCode, contentType, payload) {
-
+    res.statusCode = statusCode
+    res.setHeader('Content-Type', contentType)
+    res.end(JSON.stringify(payload))
 }
