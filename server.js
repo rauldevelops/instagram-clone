7a93bb14df8cb06a39833c5f42d97146a7e1ca4d
Raul Develops <124627746+rauldevelops@users.noreply.github.com>
2025-12-20 21:46:24 -0500

    updated server with static content

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 import http from 'http'
 
-const PORT = 8000
+const PORT = 5500
+
+const __dirname = import.meta.dirname
 
 const server = http.createServer((req, res) => {
-    
     if (req.url === '/') {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/html')
-    res.end('<h1>Hello, World!</h1>')
+        serveStatic(req, res, __dirname)
     }
 })
 
