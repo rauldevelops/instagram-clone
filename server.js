4711182ac00dd680d3e62ddf95645cdc3ff62c26
Raul Develops <124627746+rauldevelops@users.noreply.github.com>
2025-12-29 15:48:31 -0500

    migrated app from vanilla node to express and set up express.static for public folder

--- server.js
+++ server.js
@@ -1,27 +1,11 @@
-import http from 'http'
-import path from 'node:path'
-import fs from 'node:fs/promises'
-import { serveStatic } from './utils/serveStatic.js'
-import { handleGet } from './handlers/routeHandlers.js'
+import express from 'express'
 
 const PORT = 8000
 
-const __dirname = import.meta.dirname
+const app = express()
 
-console.log('__dirname:', __dirname)
+app.use( express.static('public'))
 
-const server = http.createServer( async (req, res) => {
-    console.log (req.method, req.url)
-    if (req.url === '/api') {
-        if (req.method === 'GET') {
-            return await handleGet(res)
-        }
-    }
-        else if (req.url === '/') {
-        serveStatic(req, res, __dirname)
-    }
-})
-
-server.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`)
 })
--- utils/sendResponse.js
+++
@@ -1,8 +0,0 @@
-export async function sendResponse(res, statusCode, contentType, payload) {
-    res.statusCode = statusCode
-    res.setHeader('Content-Type', contentType)
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
-    res.end(JSON.stringify(payload))
-}
